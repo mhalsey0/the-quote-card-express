64bf2aef75ca50f793ff2309421aa1a32106dc2a
Mike Halsey <mhalsey0@gmail.com>
2025-06-17 18:48:18 -0400

    connected to the unsplash API and added it to the front end.

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,16 @@
-"use strict"
+async function getRandomImage() {
+    const client_id = "YfsgvlpVilK5evmxteN6mTsH0US70CmpjvJJ5nHnjqc";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-const elements = {
-    quote: document.getElementById("quote"),
-    author: document.getElementById("author"),
-};
-
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-    },
-
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-
-    {
-        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
     }
-];
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
