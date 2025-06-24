4e614d6b71b61928cf7bc306ab469ec6f9ad975a
Mike Halsey <mhalsey0@gmail.com>
2025-06-24 18:38:39 -0400

    added cors and dotenv to protect the endpoint

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,12 @@
 async function getRandomImage() {
-    const client_id = "YfsgvlpVilK5evmxteN6mTsH0US70CmpjvJJ5nHnjqc";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
